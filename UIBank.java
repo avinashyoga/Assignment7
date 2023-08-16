39ad758af28d62b4e6afc63e33713545b21b6fa2
avinashyoga <139771671+avinashyoga@users.noreply.github.com>
2023-08-16 13:37:23 +0530

    Add files via upload

---
+++ UIBank.java
@@ -0,0 +1,59 @@
+package Assiment5;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class UIBank {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("https://uibank.uipath.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.findElement(By.xpath("//button[@class='btn btn-white btn-xl get-started-button']")).click();
+		
+		driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Avinash");
+		
+		WebElement findElement = driver.findElement(By.xpath("//select[@id='title']"));
+		
+		Select Title=new Select(findElement);
+		
+		Title.selectByVisibleText("Mr");
+		
+		driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("Y");
+		
+		WebElement findElement2 = driver.findElement(By.xpath("//select[@name='gender']"));
+		
+		Select Gender=new Select(findElement2);
+		
+		Gender.selectByVisibleText("Male");
+		
+		WebElement findElement3 = driver.findElement(By.xpath("//select[@name='employmentStatus']"));
+		
+		Select Employee=new Select(findElement3);
+	
+		Employee.selectByVisibleText("Full-time");
+		
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("Avinash");
+		
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("avinashyoga2@gmail.com");
+		
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Avinash@950025");
+		
+		System.out.println("Title of Page: " + driver.getTitle());
+		
+		driver.close();
+		
+		
+		
+		
+		
+	}
+
+}
